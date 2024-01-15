e9c813071d71add47728a83b42ce97f6d2751651
smylenetwork <156674787+smylenetwork@users.noreply.github.com>
2024-01-15 16:29:33 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 window.function = async function () {
   // Use fetch API to get IP address
-  const response = await fetch('https://geo.ipify.org/api/v2/country?apiKey=at_lFb5Bim8HbHHiqgGgug1yse75orRv');
+  const response = await fetch('https://ipapi.co/json/');
   const data = await response.json();
 
   // Return the IP address
-  return data.region;
+  return data.city;
 }
